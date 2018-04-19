abf65ca4035dec6c8d1fab547c2a40a20f183de6
eunaumtenhoid <eunaumtenhoid@outlook.com>
2018-04-19 10:09:26 -0300

    Traducao ADD

---
+++ language/pt_br/info_acp_movetopicswhenlocked.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ *
+ * @package phpBB Extension - Move Topics When Locked
+ * @copyright (c) 2016 kasimi
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ * Brazilian Portuguese translation by eunaumtenhoid (c) 2018 [ver 1.0.4] (https://github.com/phpBBTraducoes)
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'LOG_MOVED_LOCKED_TOPIC' => '<strong>Tópico trancado “%s” movido automaticamente</strong><br />&raquo; de %s<br />&raquo; para %s',
+));
